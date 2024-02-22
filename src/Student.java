119e02125359b4daa7fe300e7b413140dde046cb
branhucks <branhucks@gmail.com>
2024-02-22 16:35:45 -0500

    fixed #6

---
+++ src/Student.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Student extends User {
+    private Year classYear;
+    private Advisor advisor;
+    private double gpa;
+    private Major major;
+    private String studentID;
+    private boolean hasScholarship;
+    private int majorProgress;
+    private SemesterPlan semesterPlan;
+
+    public Student(String studentID, Advisor advisor, Major major, Year classYear) {
+        this.studentID = studentID;
+        this.advisor = advisor;
+        this.major = major;
+        this.classYear = classYear;
+    }
+
+    public SemesterPlan createSemesterPlan(ArrayList<Course> courses) {
+        // TODO
+        return null;
+    }
+
+    public SemesterPlan viewSemesterPlan() {
+        // TODO
+        return null;
+    }
+
+    public SemesterPlan viewWhatIf(Major major) {
+        // TODO
+        return null;
+    }
+
+    public ArrayList<Course> selectCourses() {
+        // TODO
+        return null;
+    }
+
+    public void printTranscript() {
+        // TODO
+    }
+
+    public void viewMajorProgression() {
+        // TODO
+    }
+
+    public boolean setScholarship() {
+        // TODO
+        return false;
+    }
+
+    public Course findCourse(String courseID) {
+        // TODO
+        return null;
+    }
+
+    public Course viewCourseDetails(String courseID) {
+        // TODO
+        return null;
+    }
+
+    public ArrayList<Course> viewCompletedCourses() {
+        // TODO
+        return null;
+    }
+
+    public ArrayList<Course> viewIncompleteCourses() {
+        // Implementation here
+        return null;
+    }
+
+    public ArrayList<Course> viewInProgressCourses() {
+        // TODO
+        return null;
+    }
+
+    public boolean riskOfFailure(double gpa) {
+        // TODO
+        return false;
+    }
+}
