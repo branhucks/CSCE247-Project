21b8e67c84819c931078cc691e4207763909e7d1
Thomas Schrank <tschrank@email.sc.edu>
2024-02-22 14:13:28 -0500

    fixed #1

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -5,3 +5,19 @@ import java.util.UUID;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
+public class DataLoader {
+
+    public ArrayList<User> getUsers() {
+        
+        return;
+    }
+    public ArrayList<User> getCourses() {
+
+        return;
+    }
+    public ArrayList<Major> getMajors() {
+
+        return;
+    }
+}
