1673cfd0896946bf1157be216cbdf32876170d97
branhucks <branhucks@gmail.com>
2024-02-22 16:26:39 -0500

    fixed #9

--- src/Advisor.java
+++ src/Advisor.java
@@ -1,28 +1,33 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Advisor extends User {
     private ArrayList<Student> advisees;
 
     public Advisor(ArrayList<Student> advisees) {
-        super();
         this.advisees = advisees;
     }
 
     public Student searchStudent(String studentID) {
-        return student;
+        // TODO
+        return null;
     }
 
     public ArrayList<Student> viewAdvisees() {
-        return students;
+        // TODO
+        return this.advisees;
     }
 
     public Student selectStudentProfile(String studentID) {
-        return student;
+        // TODO
+        return null;
     }
 
     public void makeNote(Student student String note) {
-        
+        // TODO
     }
 
     public void enterGrades(Student student, ArrayList<Course> courses) {
-
+        // TODO
     }
 }
