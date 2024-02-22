f920b4cac966bc4d4b2364acb9a12018b7f9e697
branhucks <branhucks@gmail.com>
2024-02-22 16:56:59 -0500

    fixed #27

--- src/User.java
+++ src/User.java
@@ -1,35 +1,62 @@
 import java.util.UUID;
 
 public class User {
-    
     protected UUID id;
     protected String firstName;
     protected String lastName;
     protected String username;
     protected String password;
 
-    public User(String firstName, String lastName, String username, String password)
-    {
+    public User(String firstName, String lastName, String username, String password) {
+        this.id = UUID.randomUUID();
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
         this.password = password;
     }
 
-    public User(UUID id, String firstName, String lastName, String username, String password)
-    {
+    public User(UUID id, String firstName, String lastName, String username, String password) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
         this.password = password;
     }
-    public User login()
-    {
-        return new User("firstName", "lastName", "username", "password");
-    }
-    public void logout()
-    {
 
+    public UUID getID() {
+        return id;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
