5e425c73f9db6dcfb67572658cd449bdf5e9b1ef
branhucks <branhucks@gmail.com>
2024-02-24 12:32:43 -0500

    Created basic User instance

--- src/DataWriter.java
+++ src/DataWriter.java
@@ -1,15 +1,44 @@
-public class DataWriter {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-    public boolean saveUsers() {
-        //TODO
-        return true;
+public class DataWriter extends DataConstants {
+
+    public static void saveUsers() {
+        UserList users = UserList.getInstance();
+        ArrayList<User> userList = users.getUsers();
+        JSONArray jsonUsers = new JSONArray();
+
+        for (int i = 0; i < userList.size(); i++) {
+            jsonUsers.add(getUserJSON(userList.get(i)));
+        }
+
+        try (FileWriter file = new FileWriter(USER_FILE_NAME)) {
+            file.write(jsonUsers.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static JSONObject getUserJSON(User user) {
+        JSONObject userDetails = new JSONObject();
+        userDetails.put(USER_ID, user.getID().toString());
+        userDetails.put(USER_USERNAME, user.getUsername());
+        userDetails.put(USER_FIRST_NAME, user.getFirstName());
+        userDetails.put(USER_LAST_NAME, user.getLastName());
+        return userDetails;
+    }
+
     public boolean saveCourses() {
-        //TODO
+        // TODO
         return true;
     }
+
     public boolean saveMajors() {
-        //TODO
+        // TODO
         return true;
     }
 }
