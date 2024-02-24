03c3cb4f58eeb86a226eb423c5aa158a5ccab9d5
branhucks <branhucks@gmail.com>
2024-02-24 12:24:28 -0500

    Created basic User case

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -6,4 +6,28 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-public
+public class DataLoader extends DataConstants {
+
+    public static ArrayList<User> getUsers() {
+        ArrayList<User> userList = new ArrayList<User>();
+
+        try {
+            FileReader reader = new FileReader(USER_FILE_NAME);
+            JSONParser parser = new JSONParser();
+            JSONArray peopleJSON = (JSONArray) new JSONParser().parse(reader);
+
+            for (int i = 0; i < peopleJSON.size(); i++) {
+                JSONObject personJSON = (JSONObject) peopleJSON.get(i);
+                UUID id = UUID.fromString((String) personJSON.get(USER_ID));
+                String username = (String) personJSON.get(USER_USERNAME);
+                String firstName = (String) personJSON.get(USER_FIRST_NAME);
+                String lastName = (String) personJSON.get(USER_LAST_NAME);
+                userList.add(new User(id, username, firstName, lastName));
+            }
+            return userList;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
