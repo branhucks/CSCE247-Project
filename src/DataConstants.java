073f65d8bed909d80104d24e73cbe8220fce9e3e
branhucks <branhucks@gmail.com>
2024-02-24 12:16:48 -0500

    fixed #41

---
+++ src/DataConstants.java
@@ -0,0 +1,7 @@
+public abstract class DataConstants {
+    protected static final String USER_FILE_NAME = "./src/users.json";
+    protected static final String USER_ID = "id";
+    protected static final String USER_USERNAME = "username";
+    protected static final String USER_FIRST_NAME = "firstName";
+    protected static final String USER_LAST_NAME = "lastName";
+}
