ac84356131c76c68cd36c61585c74519c98266e2
Ash Jacobson <asH721123@gmail.com>
2024-02-27 13:57:57 -0500

    fixed #36

--- src/Course.java
+++ src/Course.java
@@ -35,14 +35,55 @@ public class Course {
         return this;
     }
 
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
     
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public void setRequirement(String requirement) {
+        this.requirement = requirement;
+    }
+
+    public void setSemester(Semester semester) {
+        this.semester = semester;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrerequisites(ArrayList<PrereqOptions> prerequisites) {
+        this.prerequisites = prerequisites;
+    }
+
+    public void setCorequisites(ArrayList<Course> corequisites) {
+        this.corequisites = corequisites;
+    }
+
+    public void setCreditHours(int creditHours) {
+        this.creditHours = creditHours;
+    }
+
+    public void setPassingGrade(char passingGrade) {
+        this.passingGrade = passingGrade;
+    }
+
+    private boolean checkComplete()
+    {
+        if (id!=null&&courseName!=null&&courseID!=null&&requirement!=null&&semester!=null&&description!=null&&creditHours!=null&&passingGrade!=null) {
+            
+        }
+    }
 
     public ArrayList<PrereqOptions> printPrerequisites(Course course) {
         return null;
-    }
-
-    public void setCourseID(String courseID) {
-        this.courseID = courseID;
     }
 
     public String getCourseID() {
