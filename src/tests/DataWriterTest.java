caa10781756a7bb042a6fdf6c131c353f2cd3d9a
branhucks <branhucks@gmail.com>
2024-03-19 14:15:56 -0400

    initializing testing files/folders

---
+++ src/tests/DataWriterTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DataWriterTest {
+
+}
