63f8e865e9488fbdd37fe67069239df1d5fb564f
branhucks <branhucks@gmail.com>
2024-03-30 17:09:30 -0400

    missing ;

--- src/FacadeTest.java
+++ src/FacadeTest.java
@@ -1,5 +1,4 @@
 
-
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -14,45 +13,45 @@ public class FacadeTest {
     private FACADE facade;
 
     @BeforeEach
-    public void loginTest(){
-        //Boolean check = facade.login("klong");
+    public void loginTest() {
+        // Boolean check = facade.login("klong");
         System.out.println(facade.login("klong"));
     }
 
     @AfterEach
-    public void logoutTest(){
+    public void logoutTest() {
         facade.logout();
     }
-    
+
     @Test
-    public void getMajorUUID(){
+    public void getMajorUUID() {
         Major UUIDMajor = facade.getMajorByUUID("nipNorp");
     }
 
     @Test
-    public void getCourseUUID(){
+    public void getCourseUUID() {
         Course UUIDcourse = facade.getCourseByUUID(null);
         UUIDcourse.courseID();
     }
 
     @Test
-    public void saveUsersTest(){
+    public void saveUsersTest() {
         facade.saveUsers();
     }
 
     @Test
-    public void regAdvisorTest(){
-        System.out.println( facade.registerAdvisor(null, null, null, null, null));
+    public void regAdvisorTest() {
+        System.out.println(facade.registerAdvisor(null, null, null, null, null));
     }
 
     @Test
-    public void regStudentTest(){
+    public void regStudentTest() {
         System.out.println(facade.registerStudent(null, null, null, null, null, null));
     }
 
     @Test
-    public void addCourseTest(){
-        System.out.println(facade.addCourse(null, null, null, null, null, null, 0, 0))
+    public void addCourseTest() {
+        System.out.println(facade.addCourse(null, null, null, null, null, null, 0, 0));
     }
 
     @Test
@@ -61,9 +60,8 @@ public class FacadeTest {
         System.out.println(check);
     }
 
-
     @Test
-    public void getStudentCoursesTest(){
+    public void getStudentCoursesTest() {
         ArrayList<String> stuCourses = facade.getStudentCourses();
         for (String course : stuCourses) {
             System.out.println(course);
@@ -71,29 +69,27 @@ public class FacadeTest {
     }
 
     @Test
-    public void listAdviseesTest(){
+    public void listAdviseesTest() {
         facade.listAdvisees();
     }
 
     @Test
-    public void addAdviseeTest(){
+    public void addAdviseeTest() {
         facade.addAdvisee(null);
     }
 
     @Test
-    public void viewStudentProgressTest(){
+    public void viewStudentProgressTest() {
         facade.viewStudentProgress(null);
     }
 
     @Test
-    public void makeNoteTest(){
+    public void makeNoteTest() {
         facade.makeNote(null, null);
     }
 
     @Test
-    public void viewProgressTest(){
+    public void viewProgressTest() {
         facade.viewProgress();
     }
 }
-
-
